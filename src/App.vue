e59b09335469d528c911885e47a9ea4904c9c4f9
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-07 15:33:37 -0300

    feat: create transition-group component.

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,26 @@
         </div>
 
         <div class="container">
-            <TransitionExample select transition-name="select transition"/>
+            <TransitionGroup/>
+            <!-- <TransitionExample select transition-name="select transition"/>
             <TransitionExample transition-name="fade" transition-type="transition"/>
             <TransitionExample transition-name="zoom" transition-type="transition"/>
             <TransitionExample transition-name="slide-transition" transition-type="transition"/>
             <TransitionExample transition-name="slide-animation" transition-type="animation"/>
             <TransitionExample custom-css transition-name="custom-css"/>
-            <TransitionExample custom-js transition-name="custom-js"/>
+            <TransitionExample custom-js transition-name="custom-js"/> -->
         </div>
     </div>
 </template>
 
 <script>
-import TransitionExample from './components/TransitionExample.vue'
+// import TransitionExample from './components/TransitionExample.vue'
+import TransitionGroup from './components/TransitionGroup.vue'
 
 export default {
     components: {
-        TransitionExample
+        // TransitionExample,
+        TransitionGroup
     },
 }
 </script>
---
+++ src/components/TransitionGroup.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="item">
+
+        <h3 class="font-weight-light">Tecnologias</h3>
+
+        <div class="form-group">
+            <input 
+                type="text" 
+                class="form-control" 
+                placeholder="Insira um novo item e pressione Enter"
+                @keyup.enter="adicionar"
+                ref="inputItem">
+        </div>
+
+        <transition-group>
+
+            <li 
+                class="list-group-item"
+                v-for="(tecnologia, indice) in tecnologias"
+                :key="tecnologia">
+                    <span>{{ tecnologia }}</span>
+                    <button 
+                        class="btn btn-danger btn-sm float-right"
+                        @click="remover(indice)">
+                            x
+                    </button>
+            </li>
+
+        </transition-group>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tecnologias: [
+                'Javascript',
+                'VueJs',
+                'Vuex',
+                'Vue Router'
+            ]
+        }
+    },
+
+    methods: {
+        /**
+         * @param { KeyboardEvent } event
+         */
+        adicionar(event) {
+            const novoItem = event.target.value
+            if (!novoItem) return
+            const indice = Math.floor(Math.random() * this.tecnologias.length)
+            this.tecnologias.splice(indice, 0, novoItem)
+            this.$refs.value = ''
+        },
+
+        remover(indice) {
+            this.tecnologias.splice(indice, 1)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
